e2d199c8b85ad7a5700a26d2227d5cf1054763f0
Thomas-Gr <thomas.grilletta@gmail.com>
2020-03-02 21:00:00 +0900

    Remove test file

--- src/main/java/Test.kt
+++ src/main/java/Test.kt
@@ -1,9 +1,11 @@
 fun main() {
-  val mercariData = MercariData("1", "2", "3", "4", "5", "6")
-  val mercariData2 = MercariData("12", "2", "3", "42", "5", "62")
-  val mercariData3 = MercariData("13", "2", "3", "43", "5", "63")
+  val saleFiles = listOf(
+      "1DJbhn_niubdvIR-6hKF4g5C_NyqTrjgduR5qZqEXkVQ", // Marta
+      "1qKh5GqpNxVPO2z_M7AytpDj74nX09ZxG2sJytsex8Ys", // Sabrina
+      "1rwMGZZmj4al6A4irBBdGbCUmGqdbXHDipvODvGNHHP8" // Maxime
+  )
 
-  val sheetsExporter = SheetsExporter("1iZvBQ0qfEKCDTT86DXVftPgk9gakGkoCziR9oxS9YKo")
-
-  sheetsExporter.toSheet(listOf(mercariData, mercariData2, mercariData3))
+  for (saleFile in saleFiles) {
+    SingleSaleReporter("1iZvBQ0qfEKCDTT86DXVftPgk9gakGkoCziR9oxS9YKo", saleFile).report()
+  }
 }
