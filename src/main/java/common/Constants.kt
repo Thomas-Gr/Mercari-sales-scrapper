cc0b120e55b584278a1cb4e8d59352c723b448ce
Thomas-Gr <thomas.grilletta@gmail.com>
2020-11-08 10:49:09 +0900

    Reset constant file

--- src/main/java/common/Constants.kt
+++ src/main/java/common/Constants.kt
@@ -1,7 +1,17 @@
 package common
 
-const val DRIVER = "xxx"
-const val LOCAL_EXPORT = "xxx"
-const val SPREADSHEET_ID = "xxx"
+const val DRIVER = ""
+const val LOCAL_EXPORT = ""
+const val SPREADSHEET_ID = ""
+const val SPREADSHEET_FB = ""
 
-val SALE_FILES = listOf("")
+val SALE_FILES = listOf<String>()
+
+const val FB_SALES_SHEET_ID = ""
+const val FB_SALES_RESULTS_ID = ""
+const val FB_SALES_INPUT_FOLDER = ""
+const val FB_SALES_OUTPUT_FOLDER = ""
+const val FB_SALES_MERGER_FOLDER = ""
+
+const val FB_PATH_ENTRY = ""
+const val FB_PATH_GROUP = ""
